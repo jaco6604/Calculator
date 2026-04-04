8200e1023fc294da3e761411d3643b84d95c4dd8
jaco6604 <ryanjacobson1218@gmail.com>
2026-04-04 17:39:01 -0400

    Add basic arithmetic functions: add, subtract, multiply, divide

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b){
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+function subtract(a, b){
+    return a - b;
+}
+console.log(subtract(5, 2));
+
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(7, 2));
+
+function divide(a, b){
+    return a / b;
+}
+console.log(divide(10, 2))
